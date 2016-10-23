8622f37ec52f0441137f526c784de68f53ff2531
Wojtas <wojciech.osak@o2.pl>
2016-10-24 00:01:29 +0200

    Hibernate dzialajacy, proba podlaczenia pod jsp

--- src/main/java/com/sciencepublications/controllers/HomeController.java
+++ src/main/java/com/sciencepublications/controllers/HomeController.java
@@ -1,22 +1,32 @@
 package com.sciencepublications.controllers;
 
+import com.sciencepublications.models.UserEntity;
 import com.sciencepublications.util.HibernateUtil;
 import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/")
-    public String home(){
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        System.out.println(session);
+    public String home() {
         return "test";
     }
 
-    @RequestMapping(value="/login")
-    public String login(){
+    @RequestMapping(value = "/login")
+    public String login(Model model) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        ArrayList<UserEntity> users = new ArrayList<UserEntity>(session.createCriteria(UserEntity.class).list());
+        int i = 0 ;
+        for (UserEntity userEntity : users) {
+            System.out.println(userEntity.getLogin() + "/" + userEntity.getPassword());
+            model.addAttribute("user"+i, userEntity.getLogin());
+            model.addAttribute("pass"+i, userEntity.getPassword());
+        }
         return "login";
     }
 
--- src/main/java/com/sciencepublications/util/HibernateUtil.java
+++ src/main/java/com/sciencepublications/util/HibernateUtil.java
@@ -1,6 +1,5 @@
 package com.sciencepublications.util;
 
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
@@ -8,27 +7,18 @@ import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil
 {
-    private static SessionFactory sessionFactory;
-    private static ServiceRegistry serviceRegistry;
 
-    static
-    {
-        try
-        {
-            Configuration configuration = new Configuration().configure();
-
-            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        }
-        catch (HibernateException he)
-        {
-            System.err.println("Error creating Session: " + he);
-            throw new ExceptionInInitializerError(he);
-        }
+    private static SessionFactory createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
+                configuration.getProperties()).buildServiceRegistry();
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
     }
 
     public static SessionFactory getSessionFactory()
     {
-        return sessionFactory;
+        return createSessionFactory();
     } 
 }
