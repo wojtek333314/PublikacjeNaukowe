8a62458d94c0a9688c3e238378a000c797398eaf
Wojtas <wojciech.osak@o2.pl>
2016-10-23 23:38:31 +0200

    Hibernate init

--- src/main/java/com/sciencepublications/controllers/HomeController.java
+++ src/main/java/com/sciencepublications/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.sciencepublications.controllers;
 
+import com.sciencepublications.util.HibernateUtil;
+import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,6 +10,8 @@ public class HomeController {
 
     @RequestMapping(value = "/")
     public String home(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        System.out.println(session);
         return "test";
     }
 
---
+++ src/main/java/com/sciencepublications/models/UserEntity.java
@@ -0,0 +1,63 @@
+package com.sciencepublications.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user", schema = "wojtek14_publikacjenaukowe", catalog = "")
+public class UserEntity {
+    private int id;
+    private String login;
+    private String password;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (id != that.id) return false;
+        if (login != null ? !login.equals(that.login) : that.login != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+}
